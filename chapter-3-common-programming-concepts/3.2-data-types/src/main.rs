f5b1dce5d7316951248c5ab1b25dba0eb1e6d590
Derek M Frank <derek@frank.sh>
2021-05-29 16:49:41 -0700

    chore(chapter-3.2): Work with scalar types

--- chapter-3-common-programming-concepts/3.2-data-types/src/main.rs
+++ chapter-3-common-programming-concepts/3.2-data-types/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Scalar types!");
+    let _decimal: isize = 98_222;
+    let _hex: usize = 0xff;
+    let _octal: i16 = 0o77;
+    let _binary: u16 = 0b1111_0000;
+    let _byte: u8 = b'A';
 }
