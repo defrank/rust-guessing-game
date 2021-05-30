8ed10f079753cd71b14b27b9f3dab4b7e4c386e7
Derek M Frank <derek@frank.sh>
2021-05-29 17:35:18 -0700

    refactor(chapter-3.2): Print more stuff

--- chapter-3-common-programming-concepts/3.2-data-types/src/main.rs
+++ chapter-3-common-programming-concepts/3.2-data-types/src/main.rs
@@ -1,32 +1,37 @@
 fn main() {
     println!("Scalar types!");
-    let _decimal: isize = 98_222;
-    let _hex: usize = 0xff;
-    let _octal: i16 = 0o77;
-    let _binary: u16 = 0b1111_0000;
-    let _byte: u8 = b'A';
-
+    let decimal: isize = 98_222;
+    let hex: usize = 0xff;
+    let octal: i16 = 0o77;
+    let binary: u16 = 0b1111_0000;
+    let byte: u8 = b'A';
+    println!("{} {} {} {} {}", decimal, hex, octal, binary, byte);
+    println!();
     println!("Integer overflow!");
     assert_eq!((i32::MAX - 2).checked_add(1), Some(i32::MAX - 1));
     assert_eq!((i32::MAX - 2).checked_add(3), None);
-
+    println!();
     println!("Floating-point types!");
-    let _x = 2.0;  // f64
-    let _y: f32 = 3.0;  // f32
-
+    let x = 2.0;  // f64
+    let y: f32 = 3.0;  // f32
+    println!("{} {}", x, y);
+    println!();
     println!("Numeric operations!");
-    let _sum = 5 + 10;
-    let _difference = 95.5 - 4.3;
-    let _product = 4*30;
-    let _quotient = 56.7 / 32.2;
-    let _remainder = 43 % 5;
-
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4*30;
+    let quotient = 56.7 / 32.2;
+    let remainder = 43 % 5;
+    println!("{} {} {} {} {}", sum, difference, product, quotient, remainder);
+    println!();
     println!("The boolean type!");
-    let _t = true;
-    let _f: bool = false;
-
+    let t = true;
+    let f: bool = false;
+    println!("{} {}", t, f);
+    println!();
     println!("The character type!");
     let c = 'z';
     let z = 'ℤ';
     let heart_eyed_cat = '😻';
+    println!("{} {} {}", c, z, heart_eyed_cat);
 }
