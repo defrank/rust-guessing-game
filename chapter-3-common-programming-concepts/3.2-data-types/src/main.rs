de5376ec44e14b783f1d24d98a54a05c15a47ba8
Derek M Frank <derek@frank.sh>
2021-05-29 17:44:31 -0700

    chore(chapter-3.2): Access tuple elements using a period

--- chapter-3-common-programming-concepts/3.2-data-types/src/main.rs
+++ chapter-3-common-programming-concepts/3.2-data-types/src/main.rs
@@ -39,4 +39,8 @@ fn main() {
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     let (x, y, z) = tup;
     println!("{} {} {}", x, y, z);
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+    println!("{} {} {}", five_hundred, six_point_four, one);
 }
