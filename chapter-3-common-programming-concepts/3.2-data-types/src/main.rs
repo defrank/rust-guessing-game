39f6cc28f1a8cd7106a35f317f0b2a4865337739
Derek M Frank <derek@frank.sh>
2021-05-29 17:03:21 -0700

    chore(chapter-3.2): Work with integer overflow

--- chapter-3-common-programming-concepts/3.2-data-types/src/main.rs
+++ chapter-3-common-programming-concepts/3.2-data-types/src/main.rs
@@ -5,4 +5,8 @@ fn main() {
     let _octal: i16 = 0o77;
     let _binary: u16 = 0b1111_0000;
     let _byte: u8 = b'A';
+
+    println!("Integer overflow!");
+    assert_eq!((i32::MAX - 2).checked_add(1), Some(i32::MAX - 1));
+    assert_eq!((i32::MAX - 2).checked_add(3), None);
 }
